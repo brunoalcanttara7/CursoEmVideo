624469632a54f155aa53fd8c5637c93d28ffd3fd
DT5S <brunoalcanttara@hotmail.com>
2023-06-02 16:39:37 -0300

    Commit dia 02/06

--- ex017.py
+++ ex017.py
@@ -7,5 +7,5 @@ aluno2 = input('Qual o nome do aluno 2? ')
 aluno3 = input('Qual o nome do aluno 3? ')
 aluno4 = input('Qual o nome do aluno 4? ')
 lista = [aluno1, aluno2, aluno3, aluno4]
-sorteio = random.choices(lista)
+sorteio = random.choice(lista)
 print('O aluno que irá apagar o quadro é {}'.format(sorteio))
--- ex018.py
+++ ex018.py
@@ -7,5 +7,5 @@ aluno2 = input('Qual o aluno 2? ')
 aluno3 = input('Qual o aluno 3? ')
 aluno4 = input('Qual o aluno 4? ')
 lista = [aluno1, aluno2, aluno3, aluno4]
-ordem = random.uniform(lista)
-print(ordem)
+random.shuffle(lista)
+print(lista)
