96def572a0cb0734de51aa40117da989932ff167
DT5S <brunoalcanttara@hotmail.com>
2023-06-21 16:54:43 -0300

    Commit dia 21/06

--- ex010-escape.py
+++ ex010-escape.py
@@ -1,5 +1,5 @@
 import html
-string_original = "<script>alert(1)</script"
+string_original = "<script>alert(1)</script>"
 string_escape = html.escape(string_original)
 # Exibindo o resultado
 print("String original: {}".format(string_original))
